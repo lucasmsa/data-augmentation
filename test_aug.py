27dd3deea4e7791c9d654d456cb4a1c7dd945c3f
withoutCoffe <jonasbdc1998@gmail.com>
2020-03-23 13:39:36 -0300

    almost there 2

--- test_aug.py
+++ test_aug.py
@@ -48,36 +48,42 @@ behind_ahead = r'(.+){0}(.+)'.format(pattern_agent_verb)
 search_pattern_agent_verb = re.findall(pattern_agent_verb, phrases[1])
 search_results = re.finditer(behind_ahead, phrases[1])
 
-print(search_pattern_agent_verb[0])
+#print(search_pattern_agent_verb[0])
 
 list_new_phrases_s = []
 list_new_phrases_noun = []
 
+phrases = ['OI EU ABRAÇAR TU PASSARO TU TRABALHAR EU QUIMO']
 for phrase in phrases:
 
   # resultado do regex para o padrão [agente -> verbo -> receptor]
   search_pattern_agent_verb = re.findall(pattern_agent_verb, phrase)
   # resultado do regex para o padrão [agente -> pronome -> verbo]
   search_pattern_pronoun_verb = re.findall(pattern_pronoun_verb, phrase)
-
-  print("-> ", search_pattern_agent_verb)
+  #print("--> ", search_pattern_agent_verb)
 
   if search_pattern_agent_verb:
-
     # para cada padrão encontrado na frase
     for patterns_in_phrase in range(len(search_pattern_agent_verb)):
           
       # TODO: vou receber uma frase no padrão [('EU', 'ABRAÇAR', 'TU'), ('TU', 'TRABALHAR', 'EU')], fazer um regex para 
       # TODO: reconhecer o padrão do search_pattern_agent_verb e agrupar em look-behind e look-ahead, e estes serao usados 
       # TODO: na criação da frase tratada
-
-
-      # padrão para o reconhecimento das partes de trás e da frente do padrão
-      behind_ahead = r'(.+){0}(.+)'.format(pattern_agent_verb)
+      
+      '''Transformando a padrão em search_pattern_agent_verb em string para pesquisar a parte de trás e da frente do padrão'''
+      string = ''
+      for index, part in enumerate(search_pattern_agent_verb[patterns_in_phrase]):
+          if not index:
+            string +=part
+          else:
+              string += ' '+part
+      
+      # padrão para o reconhecimento das partes de trás e da frente do padrão 
+      ###behind_ahead = r'(.+){0}(.+)'.format(pattern_agent_verb)
+      behind_ahead = r'(.+){0}(.+)'.format(string)
 
       behind_ahead_results = re.findall(behind_ahead, phrase)
-
-      print("<- ", behind_ahead_results)
+      #print("behind_ahead<-- ", behind_ahead_results)
 
       # partes de trás
       before_pattern = behind_ahead_results[0][0]
@@ -90,7 +96,6 @@ for phrase in phrases:
       combinations = product(agents_dict.keys(), agents_dict.keys(), repeat=1)
 
       for item in combinations:
-
         # criação de frases do tipo [1S, 2S ...]
         list_new_phrases_s.append(f'{before_pattern} {item[0]} {verb} {item[1]} {after_pattern}')
 
@@ -100,11 +105,9 @@ for phrase in phrases:
 
           for index_2, item_2 in enumerate(agents_dict[item[1]]):
 
-            list_new_phrases_noun.append(f'{before_pattern} {item_1} {verb} {item_2} {after_pattern}')
+            list_new_phrases_noun.append(f'{before_pattern}{item_1} {verb} {item_2}{after_pattern}')
 
     #elif search_pattern_pronoun_verb:
-        
-
-# for item in list_new_phrases_noun:
-#   print(item)
-
+for item in list_new_phrases_noun:
+    print(item)
+print(len(list_new_phrases_noun))
